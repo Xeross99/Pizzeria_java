59366a1519bf2bed025e5b1c78126b1af04d629a
Michał Krzysteczko <m.krzysteczko@icloud.com>
2024-12-09 20:25:54 +0100

    wip

--- src/App.java
+++ src/App.java
@@ -2,5 +2,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         Pizzeria pizzeria = new Pizzeria("Domino's Pizza", 10);
         pizzeria.displayInfo();
+        
+        pizzeria.updateOrders(11);
+        pizzeria.displayInfo();
     }
 }
--- src/Pizzeria.java
+++ src/Pizzeria.java
@@ -1,19 +1,20 @@
 public class Pizzeria {
-  private String name;
-  private int numberOfOrders;
+    private String name;
+    private int numberOfOrders;
 
-  public Pizzeria(String name, int numberOfOrders) {
-      this.name = name;
-      this.numberOfOrders = numberOfOrders;
-  }
+    public Pizzeria(String name, int numberOfOrders) {
+        this.name = name;
+        this.numberOfOrders = numberOfOrders;
+    }
 
-  public void displayInfo() {
-      System.out.println("Witaj w pizzerii: " + name + " !");
-      System.out.println("Liczba zamówień: " + numberOfOrders);
-  }
+    public void displayInfo() {
+        // System.out.print("\033[H\033[2J");  Czyszczenie konsoli
+        System.out.println("Witaj w pizzerii: " + name + "!");
+        System.out.println("Liczba zamówień: " + numberOfOrders);
+    }
 
-  public void updateOrders(int newOrders) {
-      this.numberOfOrders = newOrders;
-      System.out.println("Updated number of orders to: " + numberOfOrders);
-  }
+    public void updateOrders(int newOrders) {
+        this.numberOfOrders = newOrders;
+        System.out.println("Zaktualizowano liczbę zamówień do: " + numberOfOrders);
+    }
 }
